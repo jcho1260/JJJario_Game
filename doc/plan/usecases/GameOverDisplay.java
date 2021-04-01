4dcfae2f5640ade7b66a3858bdb0fbe412bd3efd
Adam Hufstetler <akh47@duke.edu>
2021-04-01 14:38:27 -0400

    updated adam use case

--- doc/plan/usecases/GameOverDisplay.java
+++ doc/plan/usecases/GameOverDisplay.java
@@ -1,7 +1,14 @@
 class GameOverView {
   private OogaView view;
+  private OOGAModel model;
+  private ResourceBundle myGameBundle;
+  private ResourceBundleParser myBundleParser = new ResourceBundleParser();
+  private Integer myScore;
 
   void main() {
-    view.endLevelView(levelOutcome, playerScore, highScores);
+    Boolean levelOutcome = model.getGameResult();
+    Map<Integer, Intger> highScoreMap = myBundleParser.extractHighScores(myGameBundle);
+
+    view.endLevelView(levelOutcome, myScore, highScores);
   }
 }
