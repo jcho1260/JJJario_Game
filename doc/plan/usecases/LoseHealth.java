769384bbd7ec821362184742a2885ad196a9f91d
Jin Cho <jc695@duke.edu>
2021-04-01 15:53:18 -0400

    change use case to use reflection and update actor handle collision

--- doc/plan/usecases/LoseHealth.java
+++ doc/plan/usecases/LoseHealth.java
@@ -1,10 +1,21 @@
 class GameObject {
   double[] location = new double[2];
   int health;
+  List<Method> collisionEffects;
 
-  public handleCollision(GameObject b) {
+  public void handleCollision(GameObject b, List<String> methods) {
+    for(String m : methods) {
+      Method m = this.getClass().getDeclaredMethod(methodName);
+      collisionEffects.add(m);
+    }
+    for(Method m : collisionEffects) {
+      m.invoke(NanoBrowserView.this);
+    }
+  }
+
+  public void incrementHealth (int change) {
     int oldHealth = health;
-    incrementHealth(-3);
+    health += change;
     notifyListeners("health", oldHealth, health);
   }
 }
