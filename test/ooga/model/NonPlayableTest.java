03f4859d693d2a1aaa611524080c424f333b95eb
Jessica Yang <jessica.yang@duke.edu>
2021-04-05 18:49:08 -0400

    Fixed flipped y-coordinates.

--- test/ooga/model/NonPlayableTest.java
+++ test/ooga/model/NonPlayableTest.java
@@ -7,8 +7,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class NonPlayableTest {
   Vector start = new Vector(0, 0);
-  Vector end = new Vector(5, 5);
-  Vector velocity = new Vector(1, 1);
+  Vector end = new Vector(5, -5);
+  Vector velocity = new Vector(1, -1);
   NonPlayable npc;
 
   @BeforeEach
@@ -23,7 +23,7 @@ public class NonPlayableTest {
   @Test
   void movementNoTurn() {
     npc.stepMovement(1, 5);
-    assertTrue(npc.getPosition().getX() == 1 && npc.getPosition().getY() == 1);
+    assertTrue(npc.getPosition().getX() == 1 && npc.getPosition().getY() == -1);
   }
 
   /**
@@ -32,10 +32,12 @@ public class NonPlayableTest {
   @Test
   void movementEndTurn() {
     npc.stepMovement(5, 5);
-    assertTrue(npc.getPosition().getX() == 5 && npc.getPosition().getY() == 5);
+    assertTrue(npc.getPosition().getX() == 5 && npc.getPosition().getY() == -5);
     npc.stepMovement(1, 5);
     npc.stepMovement(1, 5);
-    assertTrue(npc.getPosition().getX() == 5 && npc.getPosition().getY() == 5);
+    assertTrue(npc.getPosition().getX() == 5 && npc.getPosition().getY() == -5);
+    npc.stepMovement(1, 5);
+    assertTrue(npc.getPosition().getX() == 4 && npc.getPosition().getY() == -4);
   }
 
   /**
@@ -46,10 +48,12 @@ public class NonPlayableTest {
     npc.stepMovement(5, 5);
     npc.stepMovement(1, 5);
     npc.stepMovement(1, 5);
-    assertTrue(npc.getPosition().getX() == 5 && npc.getPosition().getY() == 5);
+    assertTrue(npc.getPosition().getX() == 5 && npc.getPosition().getY() == -5);
     npc.stepMovement(5, 5);
     npc.stepMovement(1, 5);
     npc.stepMovement(1, 5);
     assertTrue(npc.getPosition().getX() == 0 && npc.getPosition().getY() == 0);
+    npc.stepMovement(1, 5);
+    assertTrue(npc.getPosition().getX() == 1 && npc.getPosition().getY() == -1);
   }
 }
