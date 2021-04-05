e106257c38d1b4a23d756bc58732c75662d1dc02
Jessica Yang <jessica.yang@duke.edu>
2021-04-04 22:57:52 -0400

    Removed animations to be put in controller.

--- src/ooga/model/GameWorld.java
+++ src/ooga/model/GameWorld.java
@@ -4,9 +4,6 @@ import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.util.Duration;
 
 /**
  *
@@ -18,11 +15,8 @@ public class GameWorld {
     private List<GameObject> allActiveActors;
     private CollisionCheck collisionCheck;
 
-    private static final Timeline animation = new Timeline();
-    private static final int FRAMES_PER_SECOND = 100;
-    public static final double FRAME_ELAPSED_TIME = 1.0 / FRAMES_PER_SECOND;
-
     public static final double GAME_GRAVITY;
+    public static final double STEP_TIME;
 
     /**
      * Default constructor
@@ -35,7 +29,7 @@ public class GameWorld {
         collisionCheck = new CollisionCheck(collisionMethods, gameObjects, actors);
     }
 
-    public void stepFrame(double elapsedTime) {
+    public void stepFrame() {
         collisionCheck.detectAllCollisions();
         allGameObjects = collisionCheck.executeAllCollisions();
     }
@@ -94,13 +88,4 @@ public class GameWorld {
   public void acceptAllListeners(List<PropertyChangeListener> listeners) {
     // TODO implement here
   }
-
-  private void startAnimation() {
-    KeyFrame frame = new KeyFrame(Duration.seconds(FRAME_ELAPSED_TIME),
-        evt -> stepFrame(FRAME_ELAPSED_TIME));
-    animation.setCycleCount(Timeline.INDEFINITE);
-    animation.getKeyFrames().add(frame);
-    animation.play();
-  }
-
 }
