9b10fbbde5174b0095338879ff37ba147dd32444
Adam Hufstetler <akh47@duke.edu>
2021-04-05 20:05:06 -0400

    changed master files

--- src/ooga/view/launcher/LauncherView.java
+++ src/ooga/view/launcher/LauncherView.java
@@ -1,10 +1,20 @@
 package ooga.view.launcher;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import ooga.view.factories.RootFactory;
 
 public class LauncherView {
 
-  public LauncherView(Stage primaryStage) {
-
+  public void start(Stage primaryStage) {
+    try {
+      RootFactory rf = new RootFactory();
+      Pane root = rf.make("resources/view_resources/launcher/Launcher.XML");
+      primaryStage.setScene(new Scene(root));
+      primaryStage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
