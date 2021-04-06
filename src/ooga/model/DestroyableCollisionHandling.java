83e8e0b7f525ddedac0b7d1265cb36881b1c0e3a
Juhyoung Lee <juhyoung.lee@duke.edu>
2021-04-06 00:01:37 -0400

    bug fixed and refactored isCollision (now getCollisionMethods)

--- src/ooga/model/DestroyableCollisionHandling.java
+++ src/ooga/model/DestroyableCollisionHandling.java
@@ -10,21 +10,55 @@ public class DestroyableCollisionHandling {
   public DestroyableCollisionHandling() { }
 
   /**
-   * @param b
+   * Checks for collision between two objects and returns second object entity tags if they
+   * collided. Returns empty list if there is no collision.
+   *
+   * @param myself reference GameObject
+   * @param o secondary GameObject
+   * @return secondary GameObject's entity tags or empty list if no collision
    */
-  public List<String> isCollision(GameObject b) {
+  public List<String> determineCollisionMethods(GameObject myself, GameObject o) {
     List<String> ret = new ArrayList<>();
-    double bX = b.getPosition().getX();
-    double bY = b.getPosition().getY();
-    if (bX== position.getX() && bX<= position.getX() + size.getX()) {
-      if (bY >= position.getY() && bY <= position.getY() + size.getY()) {
-        List<String> collisionInfo = b.getEntityType();
-        Vector bDirection = b.getVelocity().multiply(new Vector(-1,-1));
-        String collisionDirection = bDirection.getDirection().toString();
-        for (String s : collisionInfo) {
-          ret.add(s + collisionDirection);
-        }
+    if (!isCollision(myself, o)) {
+      return ret;
+    }
+    return getCollisionMethods(myself, o);
+  }
+
+  private boolean isCollision(GameObject myself, GameObject o) {
+    double oX = o.getPosition().getX();
+    double oY = o.getPosition().getY();
+    Vector me = myself.getPosition();
+    Vector size = myself.getSize();
+    if (oX == me.getX() && oX <= me.getX() + size.getX()) {
+      if (oY >= me.getY() && oY <= me.getY() + size.getY()) {
+        return true;
       }
+    }
+    return false;
+  }
+
+  private List<String> getCollisionMethods(GameObject myself, GameObject o) {
+    Vector myVelocity = myself.getVelocity();
+    Vector oVelocity = o.getVelocity();
+
+    String myDirection = myVelocity.getDirection().toString();
+    String oDirection = oVelocity.getDirection().toString();
+    String collisionDirection = "";
+
+    if (myDirection.equals(oDirection)) {
+      if (!myVelocity.equals(new Vector(0,0))) {
+        collisionDirection = oDirection;
+      } else {
+        return new ArrayList<>();
+      }
+    } else {
+      collisionDirection = oVelocity.multiply(new Vector(-1,-1)).getDirection().toString();
+    }
+
+    List<String> ret = new ArrayList<>();
+    for (String s : o.getEntityType()) {
+      ret.add(s + collisionDirection);
     }
     return ret;
   }
