172588da7079ccefdd5d8f0153bf4b3d82b0f278
Jessica Yang <jessica.yang@duke.edu>
2021-04-05 23:45:07 -0400

    Implemented Observables in GameObject.

--- src/ooga/model/Observable.java
+++ src/ooga/model/Observable.java
@@ -1,27 +1,26 @@
 package ooga.model;
 
 import java.beans.PropertyChangeListener;
-import java.util.*;
+import java.util.List;
 
 /**
  * Handles listeners for observable pattern. Based off of Duvall's Observable lab example.
  */
 public interface Observable {
 
-    /**
-     * Add List of listeners.
-     *
-     * @param newListeners
-     */
-    void addMultipleListeners(List<PropertyChangeListener> newListeners);
+  /**
+   * Add List of listeners.
+   *
+   * @param newListeners
+   */
+  void addMultipleListeners(List<PropertyChangeListener> newListeners);
 
-    /**
-     * Notify added listeners of a change.
-     *
-     * @param property
-     * @param oldValue
-     * @param newValue
-     */
-    Object notifyListeners(String property, Object oldValue,Object newValue);
-
+  /**
+   * Notify added listeners of a change.
+   *
+   * @param property
+   * @param oldValue
+   * @param newValue
+   */
+  void notifyListeners(String property, Object oldValue, Object newValue);
 }
