b46da31ba2773f6cc2b01b01e3760609c9e2a845
Jin Cho <jc695@duke.edu>
2021-04-05 23:16:21 -0400

    fixed merge conflicts

--- src/ooga/model/MovingDestroyable.java
+++ src/ooga/model/MovingDestroyable.java
@@ -1,14 +1,21 @@
 package ooga.model;
 
-public class MovingDestroyable extends Destroyable {
+import java.util.List;
+
+public class MovingDestroyable extends Destroyable{
 
   /**
    * Default constructor with default lives, health values
    *
+   * @param entityTypes
+   * @param position
+   * @param id
+   * @param size
    * @param startLife
    * @param startHealth
    */
-  public MovingDestroyable(int startLife, int startHealth) {
-    super(startLife, startHealth);
+  public MovingDestroyable(List<String> entityTypes, Vector position, int id,
+      Vector size, int startLife, int startHealth) {
+    super(entityTypes, position, id, size, startLife, startHealth);
   }
 }
