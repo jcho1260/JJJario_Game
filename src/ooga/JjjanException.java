b3f11a901ed0e26a24f024c930408d22721fe716
Jin Cho <jc695@duke.edu>
2021-04-06 01:58:11 -0400

    was in wrong dir. exception implement pair program - jl840

--- src/ooga/JjjanException.java
+++ src/ooga/JjjanException.java
@@ -1,2 +1,13 @@
-package ooga;public class JjjanException {
+package ooga;
+
+public class JjjanException extends Exception {
+
+  public JjjanException(String message) {
+    super(message);
+  }
+
+  public JjjanException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
 }
