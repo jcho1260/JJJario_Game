1ea23d488b5a805fcbb2d66e9120cd67ef0c1052
Juhyoung Lee <juhyoung.lee@duke.edu>
2021-04-06 19:14:32 -0400

    turned Destroyable concrete

--- src/ooga/model/Destroyable.java
+++ src/ooga/model/Destroyable.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Queue;
 
-public abstract class Destroyable extends GameObject{
+public class Destroyable extends GameObject{
   private Queue<MethodBundle> collisionQueue;
   private DestroyableCollisionHandling collisionHandler;
   private Health health;
