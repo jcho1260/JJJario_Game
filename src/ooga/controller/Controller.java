8201849fa1254087b8d372157120a5eb16a733ad
Noah Citron <noah.citron@duke.edu>
2021-04-06 23:41:05 -0400

    flesh out controller methods

--- src/ooga/controller/Controller.java
+++ src/ooga/controller/Controller.java
@@ -1,14 +1,38 @@
 package ooga.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import javax.xml.parsers.ParserConfigurationException;
+import ooga.model.GameWorld;
+import ooga.model.MethodBundle;
+import ooga.model.Vector;
+import org.xml.sax.SAXException;
+
 public class Controller {
 
-  public Controller() {
+  private GameWorldFactory gameWorldFactory;
+  private CollisionsParser collisionsParser;
+  private GameWorld gameWorld;
+  private Vector frameSize;
+  private double frameRate;
 
+  public Controller(Vector frameSize, double frameRate) {
+    gameWorldFactory = new GameWorldFactory();
+    collisionsParser = new CollisionsParser();
+    this.frameSize =  frameSize;
+    this.frameRate = frameRate;
   }
 
-  public void startGame() {
+  public void startGame()
+      throws IOException, SAXException, ParserConfigurationException, ClassNotFoundException {
 
+    Map<String, Map<String, List<MethodBundle>>> collisions = collisionsParser.parseCollisions(new File(""));
+    gameWorldFactory.createGameWorld(new File(""), collisions, frameSize, frameRate);
   }
+
+
 
   //TODO: add listeners to view
 
