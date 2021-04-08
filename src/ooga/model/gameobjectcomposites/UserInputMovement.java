058d3c6d94d107b387a6e80d940221f6e2e7f2f5
Juhyoung Lee <juhyoung.lee@duke.edu>
2021-04-08 02:19:14 -0400

    added gravity back

--- src/ooga/model/gameobjectcomposites/UserInputMovement.java
+++ src/ooga/model/gameobjectcomposites/UserInputMovement.java
@@ -98,8 +98,7 @@ public class UserInputMovement {
     gravityLevel = gameGravity;
 
     double newX = elapsedTime * stepVelocityMagnitude.getX() * change.getX();
-    double newY = (elapsedTime * stepVelocityMagnitude.getY() * change.getY()); //
-//        + ((1 + change.getY()) * elapsedTime * gameGravity * gravityScale);
+    double newY = (elapsedTime * stepVelocityMagnitude.getY() * change.getY()) + ((1 + change.getY()) * elapsedTime * gameGravity * gravityScale);
 
     return new Vector(newX, newY);
   }
