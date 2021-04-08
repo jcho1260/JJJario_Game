738502103743193083d592c96e192ebea8f0717f
Jessica Yang <jessica.yang@duke.edu>
2021-04-08 02:56:02 -0400

    added setvelocity.

--- src/ooga/model/gameobjectcomposites/UserInputMovement.java
+++ src/ooga/model/gameobjectcomposites/UserInputMovement.java
@@ -90,7 +90,23 @@ public class UserInputMovement {
     return deltaPosition(elapsedTime, gameGravity, new Vector(-1, 0));
   }
 
-  public Vector getVelocity() {return stepVelocityMagnitude;}
+  /**
+   * Retrieves velocity.
+   *
+   * @return stepVelocityMagnitude
+   */
+  public Vector getVelocity() {
+    return stepVelocityMagnitude;
+  }
+
+  /**
+   * Sets stepVelocityMagnitude to new value.
+   *
+   * @param newVelocity
+   */
+  public void setVelocity(Vector newVelocity) {
+    stepVelocityMagnitude = newVelocity;
+  }
 
   // TODO refactor duplicate code w/ automatedmovement
   private Vector deltaPosition(double elapsedTime, double gameGravity, Vector change) {
