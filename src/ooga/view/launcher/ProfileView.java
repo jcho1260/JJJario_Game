5c392af9d5251adcd1b07644392a8045d06ed6ac
Adam Hufstetler <akh47@duke.edu>
2021-04-11 23:27:04 -0400

    started profile view

---
+++ src/ooga/view/launcher/ProfileView.java
@@ -0,0 +1,13 @@
+package ooga.view.launcher;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import ooga.controller.Controller;
+import ooga.view.factories.SceneFactory;
+
+public class ProfileView {
+
+  public void login(Controller controller) {
+
+  }
+}
