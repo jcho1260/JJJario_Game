ea138793abd0055e6829df34e0ca51f443ae14b2
Noah Citron <noah.citron@duke.edu>
2021-04-13 13:43:10 -0400

    refactor Profile

--- src/ooga/controller/Profile.java
+++ src/ooga/controller/Profile.java
@@ -1,20 +1,58 @@
 package ooga.controller;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.Statement;
+import java.io.FileOutputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.util.HashMap;
 import java.util.Map;
 import javafx.scene.input.KeyCode;
 import ooga.model.util.Action;
 
-public record Profile(String name, String picture, Map<KeyCode, Action> keybinds, Map<String, Map<Integer, Integer>> highScores) implements
-    Serializable {
+public class Profile implements Serializable, PropertyChangeListener {
+
+  private String name;
+  private String picture;
+  private Map<KeyCode, Action> keybinds;
+  private Map<String, Map<Integer, Integer>> highScores;
 
   public Profile(String name) {
-    this(
-        name,
-        "profiles/pictures/default.png",
-        Map.of(KeyCode.W, Action.UP, KeyCode.S, Action.DOWN, KeyCode.A, Action.LEFT, KeyCode.D, Action.RIGHT),
-        new HashMap<>()
-    );
+    this.name = name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setPicture(String f) {
+    this.picture = picture;
+  }
+
+  public void setKeybinds(Map<KeyCode, Action> keybinds) {
+    this.keybinds = keybinds;
+  }
+
+  public Map<KeyCode, Action> getKeybinds() {
+    return keybinds;
+  }
+
+  public void display(ProfileView pv) {
+
+  }
+
+  @Override
+  public void propertyChange(PropertyChangeEvent evt) {
+    String method = evt.getPropertyName();
+    Object[] args = new Object[]{evt.getNewValue()};
+    try {
+      new Statement(this, method, args).execute();
+      FileOutputStream f = new FileOutputStream("data/profiles/" + name + ".player");
+      ObjectOutput s = new ObjectOutputStream(f);
+      s.writeObject(this);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
