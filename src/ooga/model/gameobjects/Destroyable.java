3abe7e5daaf3dc5eca96a5d938cb77b01b0d195a
Jin Cho <jc695@duke.edu>
2021-04-19 00:55:36 -0400

    edited the mario level creation so that we can test for more collision types and objects

--- src/ooga/model/gameobjects/Destroyable.java
+++ src/ooga/model/gameobjects/Destroyable.java
@@ -26,14 +26,25 @@ public class Destroyable extends GameObject{
   }
 
   /**
-   *
+   * checks to see if destroyable is still alive
+   * @return true if alive
    */
   public boolean isAlive() { return health.isAlive(); }
 
+  /**
+   * checks to see if the collision is a small corner collision with the object and should be ignored
+   * @param o object destroyable is colliding with
+   * @return true if it is a small corner collision
+   */
   public boolean cornerCollision(GameObject o) {
     return collisionHandler.smallCorner(this, o);
   }
 
+  /**
+   *
+   * @param o
+   * @return
+   */
   public List<String> determineCollision(GameObject o) {
     return collisionHandler.determineCollisionMethods(this, o);
   }
@@ -81,6 +92,10 @@ public class Destroyable extends GameObject{
     health.incrementLives(increment);
   }
 
+  public void producePowerUp() {
+
+  }
+
   /**
    * Retrieves health for child classes.
    *
@@ -99,7 +114,12 @@ public class Destroyable extends GameObject{
     return health.getLives();
   }
 
+  /**
+   * gets the number of points a destroyable is worth once destroyed
+   * @return number of points destroyable is worth
+   */
   public double getScore() { return score; }
+
 
   public void kill() {
     notifyListeners("changeVisibility", true, false);
