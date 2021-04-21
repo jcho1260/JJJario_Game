fe1184981aa70bcc34d9efc1644df76b24791a8b
Juhyoung Lee <juhyoung.lee@duke.edu>
2021-04-21 17:58:47 -0400

    moved shoot notify

---
+++ test/ooga/model/PlayerCollisionTest.java
@@ -0,0 +1,21 @@
+package ooga.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import ooga.model.gameobjects.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PlayerCollisionTest {
+  Player player;
+
+  @BeforeEach
+  @Test
+  public void init() {
+    List<String> entityTags = new ArrayList<>();
+    entityTags.addAll();
+    try {
+      player = new Player()
+    }
+  }
+}
