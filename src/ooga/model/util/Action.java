836307af5f521cae4adfdde1db9bc9cda60a5e43
Jin Cho <jc695@duke.edu>
2021-04-29 21:15:18 -0400

    completed javadocs for util package
    Former-commit-id: bb605036abc649a1c493af1a43317487d71fd329

--- src/ooga/model/util/Action.java
+++ src/ooga/model/util/Action.java
@@ -2,6 +2,13 @@ package ooga.model.util;
 
 import java.io.Serializable;
 
+/**
+ * This is the enumerated class that defines the different actions a user can input with key presses.
+ * Within each enumerated value, there is also a direction defined when associated with a directioned action
+ * such as left and right.
+ *
+ * @author jincho juhyounglee jessicayang
+ */
 public enum Action implements Serializable {
   UP(new Vector(0, -1)),
   DOWN(new Vector(0, 1)),
@@ -13,6 +20,10 @@ public enum Action implements Serializable {
 
   private final Vector axis;
 
+  /**
+   * creates a unique Action enumerated value with a given vector defining direction when appropriate
+   * @param axis
+   */
   Action(Vector axis) {
     this.axis = axis;
   }
